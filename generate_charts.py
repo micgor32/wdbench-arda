032f377af3d340e4e0dd2d1485791342137427b2
Michal Gorlas <111281585+micgor32@users.noreply.github.com>
2024-02-18 15:00:56 +0100

    docs: update license header

--- generate_charts.py
+++ generate_charts.py
@@ -1,6 +1,6 @@
 # Script to generate the charts from benchmark file.
 #
-# Copyright (c) 2023 Michal Gorlas, Nedas Adamavicius
+# Copyright (c) 2024 Michal Gorlas, Nedas Adamavicius
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
